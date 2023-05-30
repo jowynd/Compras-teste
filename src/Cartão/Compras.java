a0aea02b310b6534fd220b5da3edb65a7d2c1bab
jowynd <jowynd12@gmail.com>
2023-05-30 12:34:32 -0300

    teste

---
+++ src/Cartão/Compras.java
@@ -0,0 +1,23 @@
+package Cartão;
+
+public class Compras {
+    private String descricao;
+    private double valor;
+
+
+    public Compras(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras{" + "descricao='" + descricao + '\'' +
+                ", valor=" + valor +
+                '}';
+    }
+}
